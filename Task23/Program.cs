76b610970e5e73e5121a25113eecef20ab8979df
Dmitry Baranouski <bardmitry@bk.ru>
2023-02-04 20:09:45 +0300

    Task23

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -1,22 +1,20 @@
 ﻿// Задача 23
-
 // Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
 Console.Clear();
 Console.WriteLaine ("Введите число ");
 int num=int.Parse (ConsoleReadline()!);
-int index=1;
-int K=1;
-concole.Write($"{num}->");
-while (index<=num)
+int index = 1;
+int K = 1;
+Concole.Write($"{num}->");
+while (index <= num)
 {
-    K=index*index*index;
+    K = index * index * index;
     console.Write($"{K}");
     if (index !=num)
-{
-    console.Write($",")
-}
-index++;
+    {   
+    Console.Write($"N");
+    }
+    index++;
 }
