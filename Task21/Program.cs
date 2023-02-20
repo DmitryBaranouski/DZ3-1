8e6f3abc165d96a7c4ea20553f854b729e43431a
Dmitry Baranouski <bardmitry@bk.ru>
2023-02-20 22:09:47 +0300

    Task21

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -1,7 +1,8 @@
 ﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
-console.clean ();
+
+Console.Clean ();
 Console.Write ("введите координаты точки Xa: ");
 int Xa=int.Parse(Console.ReadLine()!);
 Console.Write ("введите координаты точки Ya: ");
@@ -9,14 +10,14 @@ int Ya=int.Parse(Console.ReadLine()!);
 Console.Write ("введите координаты точки Za: ");
 int Za=int.Parse(Console.ReadLine()!);
 Console.Write ("введите координаты точки Xb: ");
-int Xb=int.Parse(Console.ReadLine()!);
+   int Xb=int.Parse(Console.ReadLine()!);
 Console.Write ("введите координаты точки Yb: ");
-int Yb=int.Parse(Console.ReadLine()!);
+  int Yb=int.Parse(Console.ReadLine()!);
 Console.Write ("введите координаты точки Zb: ");
-int Zb=int.Parse(Console.ReadLine()!);
-int координаты (координата, точка);
-{
-    Console.Write ($"введите координаты точки: ");
+int Zb= int.Parse(Console.ReadLine()!);
+int coord (координата, точка);
+{  
+     Console.Write ($"введите координаты точки: ");
     return Convert.ToInt16(Console.ReadLine());
 }
 double Decision(double x1, double x2, 
